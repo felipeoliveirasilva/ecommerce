6cea99f94cb8a5f2a556ab2583bfcad811d2dc23
Felipe Silva <felipeosilva@hotmail.com>
2021-09-19 03:26:49 -0300

    Correction of avro and kafka dependencies. Enable kafka checkout message

---
+++ checkout/src/main/java/br/felipeosilva/checkout/config/StreamingConfig.java
@@ -0,0 +1,13 @@
+package br.felipeosilva.checkout.config;
+
+import br.felipeosilva.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+//        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
--- checkout/src/main/java/br/felipeosilva/checkout/service/CheckoutServiceImpl.java
+++ checkout/src/main/java/br/felipeosilva/checkout/service/CheckoutServiceImpl.java
@@ -1,9 +1,12 @@
 package br.felipeosilva.checkout.service;
 
 import br.felipeosilva.checkout.entity.CheckoutEntity;
+import br.felipeosilva.checkout.event.CheckoutCreatedEvent;
 import br.felipeosilva.checkout.repository.CheckoutRepository;
 import br.felipeosilva.checkout.resource.checkout.CheckoutRequest;
+import br.felipeosilva.checkout.streaming.CheckoutCreatedSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +17,7 @@ import java.util.UUID;
 public class CheckoutServiceImpl implements CheckoutService {
 
     private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedSource checkoutCreatedSource;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
@@ -21,6 +25,14 @@ public class CheckoutServiceImpl implements CheckoutService {
                 .code(UUID.randomUUID().toString())
                 .status(CheckoutEntity.Status.CREATED)
                 .build();
-        return Optional.of(checkoutRepository.save(checkoutEntity));
+        final CheckoutEntity entity = checkoutRepository.save(checkoutEntity);
+
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus(entity.getStatus().name())
+                .build();
+                checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+
+        return Optional.of(entity);
     }
 }
